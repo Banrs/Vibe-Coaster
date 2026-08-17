b42727e844039fc943845bbb688f37d715b2585c
Daniel H <119779847+Banrs@users.noreply.github.com>
2026-08-17 21:58:22 +1000

    chore: test bounded crest-G authority in planar hill

--- .github/expand_camelback_shape_probe.py
+++ .github/expand_camelback_shape_probe.py
@@ -13,35 +13,41 @@ const INITIAL := [3.01169597 * 1.15 - 0.4, 3.62587650 * 1.06, 3.40,
 	5.2662035249371, 1.58]
 ''':
 '''const CONTROL_IDS := [
-	"pullup_s", "unload_s", "crest_s", "fall_s", "pullout_g", "release_s",
+	"pullup_s", "unload_s", "crest_s", "negative_g", "fall_s", "pullout_g",
+	"release_s",
 ]
-const LOWER := [1.5, 1.0, 0.5, 2.0, 3.0, 0.30]
-const UPPER := [3.5, 4.5, 5.0, 7.0, 7.0, 4.00]
+const LOWER := [1.5, 1.0, 0.5, -1.62, 2.0, 3.0, 0.30]
+const UPPER := [3.5, 4.5, 5.0, -1.32, 7.0, 7.0, 4.00]
 const INITIAL := [1.87949032 * 1.33555111055541, 3.01169597 * 1.15 - 0.4,
-	3.62587650 * 1.06, 3.40, 5.2662035249371, 1.58]
+	3.62587650 * 1.06, -1.55352865073772, 3.40, 5.2662035249371, 1.58]
 ''',
 'var solved := BoundedSolver.solve(residual, LOWER, UPPER, INITIAL, 299)':
-'var solved := BoundedSolver.solve(residual, LOWER, UPPER, INITIAL, 399)',
+'var solved := BoundedSolver.solve(residual, LOWER, UPPER, INITIAL, 499)',
 '''		(rise_arc - fall_arc) / 20.0,
 	]
 ''':
 '''		(rise_arc - fall_arc) / 20.0,
 		float(measured.apex_pitch_deg) / 0.25,
+		(float(controls[3]) + 1.55352865073772) / 0.10,
 	]
 ''',
-'''	var pullup_s := 1.87949032 * 1.33555111055541
+'''	var positive_g := 4.60068864065765
+	var negative_g := -1.55352865073772
+	var pullup_s := 1.87949032 * 1.33555111055541
 	var unload_s := float(controls[0])
 	var crest_s := float(controls[1])
 	var fall_s := float(controls[2])
 	var pullout_g := float(controls[3])
 	var release_s := float(controls[4])
 ''':
-'''	var pullup_s := float(controls[0])
+'''	var positive_g := 4.60068864065765
+	var pullup_s := float(controls[0])
 	var unload_s := float(controls[1])
 	var crest_s := float(controls[2])
-	var fall_s := float(controls[3])
-	var pullout_g := float(controls[4])
-	var release_s := float(controls[5])
+	var negative_g := float(controls[3])
+	var fall_s := float(controls[4])
+	var pullout_g := float(controls[5])
+	var release_s := float(controls[6])
 ''',
 }
 
